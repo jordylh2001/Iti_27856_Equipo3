fa775f81f2aebd9cd3ee86d54a1d3809476870b7
jordylh2001 <1930136@upv.edu.mx>
2022-06-12 19:23:52 -0500

    Actualziacion

---
+++ app/src/main/java/com/upv/pm_2022/AgendaSqlite.java
@@ -0,0 +1,56 @@
+package com.upv.pm_2022;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AgendaSqlite extends SQLiteOpenHelper {
+
+    //
+    //private static String DB_PATH = "/data/data/YOUR_PACKAGE/databases/";
+//	private static String DB_PATH = "/sdcard/";
+//    private static String DB_NAME = "Experimento01";
+
+    //Sentencia SQL para crear la tabla de Usuarios
+    String sqlCreate   = "CREATE TABLE Productos       (_id INTEGER PRIMARY KEY, nombre TEXT,descripcion TEXT,marca TEXT )";
+    String sqlCreate2  = "CREATE TABLE Precios     (_id INTEGER PRIMARY KEY, fecha TEXT, precio REAL, id_producto REFERENCES Productos(_id))";
+    String sqlCreate3  = "CREATE TABLE Tickets     (_id INTEGER PRIMARY KEY, fecha TEXT, id_producto REFERENCES Productos(_id))";
+
+    public AgendaSqlite(Context context, String name, CursorFactory factory,int version) {
+        super(context, name, factory, version);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Se ejecuta la sentencia SQL de creaci�n de la tabla
+        db.execSQL(sqlCreate);
+        db.execSQL(sqlCreate2);
+        db.execSQL(sqlCreate3);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //eliminamos la version anterior de la tabla
+        db.execSQL("DROP TABLE IF EXISTS Productos");
+        db.execSQL("DROP TABLE IF EXISTS Precios");
+        db.execSQL("DROP TABLE IF EXISTS Tickets");
+
+        //aqu� creamos la nueva versi�n de la tabla
+        db.execSQL(sqlCreate);
+        db.execSQL(sqlCreate2);
+        db.execSQL(sqlCreate3);
+
+    }
+
+}
---
+++ app/src/main/java/com/upv/pm_2022/MainActivity.java
@@ -0,0 +1,223 @@
+package com.upv.pm_2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    final String TABLA_PRINCIPAL = "Productos";
+    final String TABLA_SECUNDARIA= "Precios";
+    //final String TABLA_TERCIARIA= "Precios";
+    AgendaSqlite usdbh;
+    AlertDialog.Builder ADX;
+    AlertDialog AD;
+    SQLiteDatabase db;
+    int SiguienteID;
+    EditText edt1,edt2,edt3,edt4,edt5;
+    Cursor cursor;
+    final String NOMBRE_BASE_DATOS = "SuperMercado.db";
+    private TextView TV1;
+    private ArrayList<String> Products=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    private ArrayAdapter<String> adapter;
+
+    private Button BT1,BT2,BT3,BT4;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SiguienteID=0;
+        //cargar();
+        // Importante: Esto va antes de instanciar controles dentro de cada pestaña
+        // Agregar las pestañas---
+        Resources res = getResources();
+        TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec1 = tabHost.newTabSpec("");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("Add");
+
+
+        TabHost.TabSpec spec2 = tabHost.newTabSpec("");
+        spec2.setContent(R.id.tab2);
+        spec2.setIndicator("Show");
+
+        TabHost.TabSpec spec3 = tabHost.newTabSpec("");
+        spec3.setContent(R.id.tab3);
+        spec3.setIndicator("Edit");
+
+        TabHost.TabSpec spec4 = tabHost.newTabSpec("");
+        spec4.setContent(R.id.tab4);
+        spec4.setIndicator("Ticket");
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        tabHost.addTab(spec3);
+        tabHost.addTab(spec4);
+
+        edt1=(EditText)findViewById(R.id.nombre);
+        edt2=(EditText)findViewById(R.id.description);
+        edt3=(EditText)findViewById(R.id.Brand);
+        edt4=(EditText)findViewById(R.id.Date);
+        edt5=(EditText)findViewById(R.id.Price);
+        TV1=(TextView)findViewById(R.id.TV2);
+        listView = (ListView)findViewById(R.id.Listview1);
+        ADX = new AlertDialog.Builder(this);
+        AD = ADX.create();
+        String ArchivoDB = NOMBRE_BASE_DATOS;
+        // Guarda el archivo de labase de datos en el directorio RAIZ (o cualquier ruta de usuario)
+        //String ArchivoDB = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+NOMBRE_BASE_DATOS;
+
+        usdbh = new AgendaSqlite(this, ArchivoDB, null, 1);
+        db = usdbh.getWritableDatabase();
+        /*
+        adapter=new ArrayAdapter<String>
+                (getApplicationContext(),android.R.layout.simple_list_item_1,Products);
+        listView.setAdapter(adapter );
+
+         */
+        BT1 = (Button) findViewById (R.id.addProduct);
+        BT1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+
+
+                //Si hemos abierto correctamente la base de datos
+                if(db != null)
+                {
+                    Toast.makeText(MainActivity.this, "Insert" ,Toast.LENGTH_SHORT).show();
+                    //Generamos los datos
+                    int codigo = SiguienteID;
+                    ContentValues values = new ContentValues();
+                    values.put("nombre",edt1.getText().toString());
+                    values.put("descripcion",edt2.getText().toString());
+                    values.put("marca",edt3.getText().toString());
+                    db.insert(TABLA_PRINCIPAL,null,values);
+                    ConsultaTabla_ActualizaControl();
+                    AD.setMessage("Insertando un producto");
+                    AD.show();
+                }
+
+            }
+        });
+        BT2 = (Button) findViewById (R.id.loadProducts);
+        BT2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                //Si hemos abierto correctamente la base de datos
+                if(db != null){
+                    mostrar();
+                    adapter=new ArrayAdapter<String>
+                            (getApplicationContext(),android.R.layout.simple_list_item_1,Products);
+                    listView.setAdapter(adapter );
+                }
+
+            }
+        });
+
+        //adapter.notifyDataSetChanged();
+
+    }
+
+    private void cargar() {
+        String C1, C2, C3,C4;
+        String Fin="";
+        cursor = db.rawQuery("select * from "+TABLA_PRINCIPAL, null);
+
+        if (cursor.getCount() != 0) {
+            if (cursor.moveToFirst()) {
+                do {
+                    C1 = cursor.getString(cursor
+                            .getColumnIndex("_id"));
+
+                    C2 = cursor.getString(cursor
+                            .getColumnIndex("nombre"));
+                    C3 = cursor.getString(cursor
+                            .getColumnIndex("descripcion"));
+                    C4 = cursor.getString(cursor
+                            .getColumnIndex("marca"));
+
+                    Products.add(C1 + "-" + C2 + "-" + C3 + "-" + C4);
+
+                } while (cursor.moveToNext());
+            }
+        }
+        cursor.close();
+    }
+
+    private void mostrar() {
+        String C1, C2, C3,C4;
+        String Fin="";
+        cursor = db.rawQuery("select * from "+TABLA_PRINCIPAL, null);
+        Products.clear();
+        if (cursor.getCount() != 0) {
+            if (cursor.moveToFirst()) {
+                do {
+                    C1 = cursor.getString(cursor
+                            .getColumnIndex("_id"));
+
+                    C2 = cursor.getString(cursor
+                            .getColumnIndex("nombre"));
+                    C3 = cursor.getString(cursor
+                            .getColumnIndex("descripcion"));
+                    C4 = cursor.getString(cursor
+                            .getColumnIndex("marca"));
+
+                    Products.add(C1 + "-" + C2 + "-" + C3 + "-" + C4);
+
+                } while (cursor.moveToNext());
+            }
+        }
+        cursor.close();
+    }
+
+    void ConsultaTabla_ActualizaControl ()
+    {
+        String C1, C2, C3,C4;
+        String Fin="";
+        cursor = db.rawQuery("select * from "+TABLA_PRINCIPAL, null);
+
+        if (cursor.getCount() != 0) {
+            if (cursor.moveToFirst()) {
+                do {
+                    C1 = cursor.getString(cursor
+                            .getColumnIndex("_id"));
+
+                    C2 = cursor.getString(cursor
+                            .getColumnIndex("nombre"));
+                    C3 = cursor.getString(cursor
+                            .getColumnIndex("descripcion"));
+                    C4 = cursor.getString(cursor
+                            .getColumnIndex("marca"));
+
+                    Fin += C1 + "-" + C2 + "-" + C3 + "-" + C4 + "\n";
+
+                } while (cursor.moveToNext());
+            }
+            TV1.setText(Fin);
+        }
+        cursor.close();
+    }
+}
